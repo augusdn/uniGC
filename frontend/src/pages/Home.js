58c8958c4c134c43db8c5def43eaf232153028dc
Irfan Zainudin <m.irfan.zain@gmail.com>
2020-09-13 06:31:23 +1000

    implemented subject areas at homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,33 +1,66 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 const listofSubjects = ["ACCT","BABS","CDEV","DATA","ECON","FINS","GBAT","HDAT","IDES","JURD","LAND"];
 
-function Home() {
-  return (
-    <main>
-      <form className="Search-container">
-        <div className="form-group Search-group-chat">
-          <label form="search-group-chat">Group Chat Search</label>
-          <input type="search" className="form-control" id="search-group-chat" aria-describedby="searchHelp"/>
-          <small id="searchHelp" className="form-text text-muted">Maybe the group chat you're interested in already exists?</small>
-        </div>
-      </form>
-      <h2 className="Section-header">Subject Areas</h2>
-      <div className="Subject-list Marginal-container">
-        { listofSubjects.map(subj => {
-          return (
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{ subj }</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      redirect: false,
+      subjects: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://asia-east2-unigc-eea69.cloudfunctions.net/api/subjects")
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          subjects: data.subjects
+        })
+      })
+  }
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to="/search" />
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <form className="Search-container" onSubmit={this.setRedirect}>
+          { this.renderRedirect() }
+          <div className="form-group Search-group-chat">
+            <label form="search-group-chat">Group Chat Search</label>
+            <input type="search" className="form-control" id="search-group-chat" aria-describedby="searchHelp"/>
+            <small id="searchHelp" className="form-text text-muted">Maybe the group chat you're interested in already exists?</small>
+          </div>
+        </form>
+        <h2 className="Section-header">Subject Areas</h2>
+        <div className="Subject-list Marginal-container">
+          { this.state.subjects.map(subj => {
+            return (
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{ subj.code }: { subj.name }</h5>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
               </div>
-            </div>
-          );
-        }) }
-      </div>
-    </main>
-  );
+            );
+          }) }
+        </div>
+      </main>
+    );
+  }
 }
 
 export default Home;
