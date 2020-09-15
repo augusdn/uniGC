64c96a25d45d0533721f5dd571a17a8ef4fa5a89
Augustine Lee <augustine.lee@student.unsw.edu.au>
2020-09-15 19:31:06 +1000

    temp commit

--- frontend/src/pages/AddChat.js
+++ frontend/src/pages/AddChat.js
@@ -10,8 +10,11 @@ import firebase from "../components/firebase/firebase";
 export default function AddChat() {
   const [options, setOptions] = React.useState([]);
   const [input, setInput] = React.useState([]);
+  const [url, setUrl] = React.useState([]);
+  const [cError, setCError] = React.useState([]);
+  const [uError, setUError] = React.useState([]);
 
-  function handleChange(term) {
+  function courseSearch(term) {
     if (term) {
       const endpoint = 'https://asia-east2-unigc-eea69.cloudfunctions.net/api/search/'+term;
       console.log(endpoint)
@@ -28,6 +31,10 @@ export default function AddChat() {
     }
   }
 
+  function courseChange(value) {
+    console.log(value);
+  }
+
   const submitHandler = e => {
     e.preventDefault();
     console.log(firebase.auth().currentUser);
@@ -41,27 +48,33 @@ export default function AddChat() {
     }
   }
 
+  const validCourseRegex = RegExp(/^[a-zA-Z]{4}[0-9]{4}$/i);
+
   return (
     <form className="Add-chat-container" onSubmit={submitHandler}>
       <div className="form-group Search-group-chat">
         <h2>Add Group Chat</h2>
         <Autocomplete
-          id="search-box"
+          id="course"
           // disable onInputChange for less function calls
           // onInputChange={(event, value) => handleChange(value)}
           // enable below for less function calls
           options={courses}
+          name="course"
           // options={options}
-          onInputChange ={(event,value) => setInput(value)}
+          // onInputChange ={(event,value) => setInput(value)}
+          onInputChange ={(event,value) => courseChange(value)}
 
           getOptionLabel={(option) => option.code}
           renderInput={(params) => <TextField {...params} label="Course Search(beta)" variant="outlined" />}
-          // renderOption={(option) => <Link to={'/course/' + option.code} className="Course-links">{ option.code }</Link>}
+          renderOption={(option) =>  option.code }
         />
+        {cError.length > 0 && 
+                <span className='error'>{cError}</span>}
         <div className="Chat-id">
           <span className="Chat-id-desktop">
             www.facebook.com/messages/t/
-            <TextField id="chatId" label="Chat ID" variant="outlined" />
+            <TextField id="chatId"  onChange ={(event,value) => console.log("a")} label="Chat ID" variant="outlined" />
           </span>
         </div>
         <div className="Add-chat">
